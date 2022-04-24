b30479703708493c3ecdff2dde310b02e3756aee
Leticia <leticiancalves1@gmail.com>
2022-04-23 22:41:42 -0300

    vue config

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 module.exports = {
-  publicPath: '/portfolio-vue',
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio-vue/' : '/',
   configureWebpack: {
     module: {
       rules: [
